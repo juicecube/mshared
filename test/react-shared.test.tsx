ee3228d49efc6d0717b36f8f4e3452c888857599
罗国雄 <1025558554@qq.com>
2021-04-29 21:07:25 +0800

    feat: 单元测试

---
+++ test/react-shared.test.tsx
@@ -0,0 +1,3 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import * as rtl from '@testing-library/react';
--- test/share.test.ts
+++ test/share.test.ts
@@ -1,27 +1,55 @@
 import { Shared } from '../src';
 type SharedList = 'child' | 'main'
 
-const mainShared = new Shared<SharedList>({
-  storeName: 'main',
-  initStore: {
-    count: 0,
-  },
-  type: 'global',
-});
-
-const childSharedd = new Shared<SharedList>({
-  storeName: 'child',
-  initStore: {
-    count: 1,
-  },
-});
-
 test('share', () => {
-  childSharedd.setGlobalShare(mainShared);
+  const mainShared = new Shared<SharedList>({
+    storeName: 'main',
+    initStore: {
+      count: 0,
+    },
+    type: 'global',
+  });
+
+  const childShared = new Shared<SharedList>({
+    storeName: 'child',
+    initStore: {
+      count: 1,
+    },
+  });
+
+  childShared.setGlobalShare(mainShared);
   mainShared.setStore('main', { count: 1 });
   mainShared.subscribe((state) => {
     expect(state.main.count).toBe(1);
   });
-  expect(childSharedd.getStore().main.count).toBe(1);
-  expect(childSharedd.getStore().child.count).toBe(1);
+  expect(childShared.getStore().main.count).toBe(1);
+  expect(childShared.getStore().child.count).toBe(1);
 });
+
+test('unsubscribe', () => {
+  const shared = new Shared<SharedList>({
+    storeName: 'child',
+    initStore: {
+      count: 1,
+    },
+  });
+
+  const mainShared = new Shared<SharedList>({
+    storeName: 'main',
+    initStore: {
+      count: 0,
+    },
+    type: 'global',
+  });
+
+  const listenerA = jest.fn();
+  const listenerB = jest.fn();
+  shared.setGlobalShare(mainShared);
+  shared.subscribe(listenerA);
+  mainShared.subscribe(listenerB);
+  shared.unSubscribe();
+  shared.setStore('child', {});
+  expect(listenerA.mock.calls.length).toBe(0);
+  expect(listenerB.mock.calls.length).toBe(1);
+});
+
